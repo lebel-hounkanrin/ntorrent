c1f488d0862af8a067aa448c9b6b5d13c7ee0be8
neil <lebelhounkanrin@gmail.com>
2024-09-25 11:51:03 +0200

    feat: test udp connection

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 "use strict"
 
 import * as fs from 'fs';
+import * as dgram from "node:dgram";
 import {default as bencode }  from 'bencode';
-const torrent = bencode.decode(fs.readFileSync("./puppy.torrent"));
+import {Buffer} from 'buffer';
+import {URL} from "node:url";
+const torrent = bencode.decode(fs.readFileSync("./puppy.torrent"), undefined , undefined , "utf-8");
+
+const url = new URL(torrent.announce.toString("utf-8"));
+const socket = dgram.createSocket("udp4");
+const msg = Buffer.from("hello", "utf-8");
+
+
+socket.send(msg, 0, msg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+    console.log('message is', msg);
+});

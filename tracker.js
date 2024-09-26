f312cc5815c853170ca5344e18ef14d5c47deb8f
neil <lebelhounkanrin@gmail.com>
2024-09-26 17:50:51 +0200

    feat: format and parse message

--- tracker.js
+++ tracker.js
@@ -4,6 +4,9 @@ import * as dgram from "node:dgram";
 import {URL} from "node:url";
 import * as crypto from "node:crypto";
 import * as buffer from "node:buffer";
+import {genId} from "./utils.js";
+import bencode from "bencode";
+import * as bignum from "bignum";
 
 const getPeers = (torrent, callback) => {
     const socket = dgram.createSocket("udp4");
@@ -26,18 +29,52 @@ const udpSend = (socket, msg, rawUrl, cb) => {
     socket.send(msg, 0, msg.length, url.port, url.host, cb);
 }
 
-const respType = (resp) => {}
+const respType = (resp) => {
+    return resp.readUInt32BE(0) === 0 ?  "connect" : "announce";
+}
 
 const buildConnReq = (connResp) => {
 }
 
-const parseConnResp = (resp) => {
-}
+const buildAnnounceReq = (connId, torrent, port=6881) => {
+    const buffer = Buffer.allocUnsafe(98);
 
-const buildAnnounceReq = (connId) => {
+    connId.copy(buffer, 0);
+    buffer.writeUInt32BE(1, 8);
+    crypto.randomBytes(4).copy(buffer, 12);
+    infoHash(torrent).copy(buffer, 64);
+    genId().copy(buffer, 36);
+    Buffer.allocUnsafe(8).copy(buffer, 56);
+    size(torrent).copy(buffer, 64);
+    Buffer.alloc(8).copy(buffer, 72);
+    buffer.writeUInt32BE(0, 80);
+    buffer.writeUInt32BE(0, 80);
+    crypto.randomBytes(4).copy(buffer, 88);
+    buffer.writeUInt32BE(-1, 92);
+    buffer.writeUInt32BE(port, 96);
 }
 
 const parseAnnounceResp = (resp) => {
+    const group = (iterable, groupSize) => {
+        let groups = [];
+        for (let i = 0; i < iterable.length; i += groupSize) {
+            groups.push(iterable.slice(i, i + groupSize));
+        }
+        return groups;
+    }
+
+    return {
+        action: resp.readUInt32BE(0),
+        transactionId: resp.readUInt32BE(4),
+        leechers: resp.readUInt32BE(8),
+        seeders: resp.readUInt32BE(12),
+        peers: group(resp.slice(20), 6).map(address => {
+            return {
+                ip: address.slice(0, 4).join('.'),
+                port: address.readUInt16BE(4)
+            }
+        })
+    }
 }
 
 const createReq = () => {
@@ -51,4 +88,20 @@ const createReq = () => {
     return buffer;
 }
 
+const parseConnResp = (res) => {
+    return {
+        action: res.readUInt32BE(0),
+        transactionId: res.readUInt32BE(4),
+        connectionId: res.slice(8)
+    }
+}
+
+const infoHash =  (torrent) => {
+    const info = bencode.encode(torrent.info)
+    return crypto.createHash("sha1").update(info).digest();
+}
+const size =  (torrent) => {
+    const size = torrent.info.files ? torrent.info.map(f => f.length).reduce((a, b) => a+b) : torrent.info.length;
+    return bignum.toBuffer(size, {size: 8})
+}
 export  {getPeers}
